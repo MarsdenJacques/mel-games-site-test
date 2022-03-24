c4a3bb4b4c7ad9458b328c8ef7a34fb5a1b84b3c
MarsdenJacques <marsdenjacques@gmail.com>
2022-03-24 16:30:52 +0900

    Student has running stuff

--- app/routes/student/$id.jsx
+++ app/routes/student/$id.jsx
@@ -1,6 +1,8 @@
 import { Lessons, LessonPlayers, LessonBlocks, Blocks } from "~/dummydata"
+import { useState } from 'react'
 import { json, Link, useLoaderData } from "remix";
 import LessonPlan from "~/components/lessonplan";
+import Button from "~/components/button";
 
 export const loader = async ({ params }) => {
   return json(params.id);
@@ -9,6 +11,8 @@ export const loader = async ({ params }) => {
 export default function Student(){
 
     const studentId = useLoaderData();
+
+    const [gameText, setGameText] = useState('HAPPY VALLEY')
 
     function GetLessonBlocks(lesson){
         let filteredLessonBlocks = LessonBlocks.filter(element=>element.lesson == lesson.id)
@@ -33,18 +37,31 @@ export default function Student(){
         return populatedLessons
     }
 
+    function PlayLesson(lesson){
+        setGameText('Running: ' + lesson.title)
+    }
+
     const populatedLessons = GetStudentLessons()
 
     return(
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingBlock: '10px', paddingInline: '10px'}}>
-            <Link to={'/'}>Home</Link>
+            <div style={{width: '100%'}}><Link to={'/'}>Home</Link></div>
             <div style={{width: '400px', height: '400px', border: 'solid black', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                HAPPY VALLEY
+                {gameText}
             </div>
-            <div>
+            <div style={{paddingBlock:'15px'}}>
+                <h3 style={{textAlign: 'center'}}>Assigned Lessons</h3>
                 {populatedLessons.map((lesson, index)=>{
-                    return <LessonPlan lesson={lesson} EditCallback={()=>{return}} RemoveBlockCallback={()=>{return}} editable={false} editing={false} key={index}/>
+                    return(
+                        <div key={index} style={{backgroundColor: 'LightGray', border: 'solid black', marginBlock: '5px'}}>
+                            <LessonPlan lesson={lesson} EditCallback={()=>{return}} RemoveBlockCallback={()=>{return}} editable={false} editing={false} />
+                            <Button text={'run'} Callback={()=>PlayLesson(lesson)} />
+                        </div>
+                    )
                 })}
+            </div>
+            <div style={{paddingBlock:'15px'}}>
+                <h3 style={{textAlign: 'center'}}>Search Lessons and Activities</h3>
             </div>
         </div>
     )
