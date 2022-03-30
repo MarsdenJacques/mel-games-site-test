f49892da8a9385ea0c8112c89896eeb5c17a1ff3
MarsdenJacques <marsdenjacques@gmail.com>
2022-03-30 20:41:13 +0900

    okay

--- app/components/lessonblock.jsx
+++ app/components/lessonblock.jsx
@@ -5,6 +5,8 @@ export default function LessonBlock({block, EditCallback, AddCallback, RemoveCal
 
     let safeIndex = index
 
+    console.log(block)
+
     if(index === undefined){
         if(!editable && activeLesson) return
         safeIndex = 0
@@ -12,12 +14,14 @@ export default function LessonBlock({block, EditCallback, AddCallback, RemoveCal
     if(block === undefined) return <div></div>
     const blockData = block.id === undefined ? Blocks.find(element=>element.id === block) : block;
 
+    console.log(blockData)
+
     return(
         <div style={{marginBlock: '20px', outline: 'solid black', paddingInline: '5px', paddingBlock: '2px'}}>
-            <p>{blockData.title}</p>
-            <p>{blockData.type}</p>
-            <p>{blockData.text}</p>
-            <p>{blockData.duration}</p>
+            <p>{block.title}</p>
+            <p>{block.type}</p>
+            <p>{block.text}</p>
+            <p>{block.duration}</p>
             {editable ? 
             <div>
                 <Button text={'edit'} Callback={()=>EditCallback(block)}/>
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,21 @@
 import { Link } from "remix";
+import Button from "~/components/button";
 export default function Index() {
 
+
+
+  function Test(){
+    fetch('https://bokoco.com/api/mel/v1/lessons',{
+        method: 'GET',
+    headers: new Headers({
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo3N30.Pu-RlATReyR-V3-u4_5bwvRao50NCNnEYPCJHF1rrKY'}), 
+}).then((res)=>res.json()).then((json)=>console.log(json))
+  }
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", display: 'flex', flexDirection: 'horizontal' }}>
       <Link to={'/teacher'}>Teacher</Link>
       <Link to={'/student'}>Student</Link>
+      <Button text={'test'} Callback={Test} />
     </div>
   );
 }
