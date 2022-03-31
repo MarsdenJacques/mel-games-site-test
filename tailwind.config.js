acf09c52b4fd5697a261a7f1a64db95ba68bfbab
MarsdenJacques <marsdenjacques@gmail.com>
2022-03-31 11:27:25 +0900

    styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vag: ["VAG Rounded", "display"],
+      },
+    },
+  },
+  plugins: [],
+};
